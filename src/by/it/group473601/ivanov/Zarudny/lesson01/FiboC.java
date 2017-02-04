fba59b5f3ca795d0739f73e554fcf09c749f9772
zaruda <zaruda46@gmail.com>
2017-02-04 23:46:48 +0300

    third task is done. Fixed first test for this task(m=2)

---
+++ src/by/it/group473601/ivanov/Zarudny/lesson01/FiboC.java
@@ -0,0 +1,49 @@
+package by.it.group473601.ivanov.Zarudny.lesson01;
+
+import java.util.ArrayList;/*
+ * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
+ * необходимо найти остаток от деления n-го числа Фибоначчи на m.
+ * время расчета должно быть не более 2 секунд
+ */
+
+public class FiboC {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        FiboC fibo = new FiboC();
+        int n = 10;
+        int m = 2;
+        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
+    }
+
+    long fasterC(long n, int m) {
+        //решение практически невозможно найти интуитивно
+        //вам потребуется дополнительный поиск информации
+        //см. период Пизано
+        ArrayList<Long> s = getSequencePeriod(m);
+        long period = s.size() - 2; // находим период Пизано
+        int val = (int)(n % period);
+        return s.get(val);
+
+    }
+    private static ArrayList<Long> getSequencePeriod(int m){
+        ArrayList<Long> s = new ArrayList<>();
+        s.add((long)0);
+        s.add((long)1);
+        for(int i = 2; i < m * 6; i++){
+            s.add((s.get(i - 1) + s.get(i - 2)) % m);
+            if(s.get(i) == 1 && s.get(i-1) == 0){
+                break;
+            }
+        }
+        return s;
+    }
+
+
+}
+

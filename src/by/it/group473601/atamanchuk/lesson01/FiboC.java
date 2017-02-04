8d6078ce54262175edb7db2499c8bbdcbace87df
CHUKIN <dmitriyatamanchuk@gmail.com>
2017-02-04 23:28:13 +0300

    Complete C task and change test

--- src/by/it/group473601/atamanchuk/lesson01/FiboC.java
+++ src/by/it/group473601/atamanchuk/lesson01/FiboC.java
@@ -1,6 +1,8 @@
 package by.it.group473601.atamanchuk.lesson01;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
@@ -27,8 +29,34 @@ public class FiboC {
         //решение практически невозможно найти интуитивно
         //вам потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
+
+        ArrayList<Long> cached = new ArrayList<Long>();
+        long fibPrev = 0;
+        long fib = 1;
+        cached.add(fibPrev);
+        cached.add(fib);
+        for (int i = 2; i < n + 1; i++) {
+            long fibOld = fib;
+            fib = (fib + fibPrev) % m;
+            fibPrev = fibOld;
+            if(fibPrev==0&&fib==1)
+            {
+                cached.remove(cached.size()-1);
+                break;
+            }
+            else
+            {
+                cached.add(fib);
+            }
+
+        }
+
+        int offset = (int) (n%cached.size());
+        return cached.get(offset);
+
+
     }
+
 
 
 }
--- src/by/it/group473601/atamanchuk/lesson01/FiboTest.java
+++ src/by/it/group473601/atamanchuk/lesson01/FiboTest.java
@@ -36,7 +36,7 @@ public class FiboTest {
     @Test(timeout = 2000)
     public void fasterC() throws Exception {
         FiboC fibo=new FiboC();
-        assertTrue("fasterC failed 1", fibo.fasterC(10,1)==1L);
+        assertTrue("fasterC failed 1", fibo.fasterC(10,1)==0L);
         assertTrue("fasterC failed 2", fibo.fasterC(1,2)==1L);
         assertTrue("fasterC failed 3", fibo.fasterC(999999999,321)==34L);
     }

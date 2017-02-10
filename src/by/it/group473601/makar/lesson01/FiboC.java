38d40bcb3c04137a1966740dc96474659b3d587c
Makar Sasha <sasamakar@yandex.ru>
2017-02-10 21:57:29 +0400

    Fibo C - done

--- src/by/it/group473601/makar/lesson01/FiboC.java
+++ src/by/it/group473601/makar/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group473601.makar.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.util.ArrayList;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -21,11 +23,27 @@ public class FiboC {
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
+    ArrayList<Long> getPeriod(int m){
+        ArrayList<Long> s = new ArrayList();
+        s.add((long)0);
+        s.add((long)1);
+        for(int i = 2; i < m * 6; i++){
+            s.add((s.get(i - 1) + s.get(i - 2)) % m);
+            if(s.get(i) == 1 && s.get(i-1) == 0){
+                break;
+            }
+        }
+        return s;
+    }
+
     long fasterC(long n, int m) {
         //решение практически невозможно найти интуитивно
         //вам потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
+        ArrayList<Long> s = getPeriod(m);
+        long period = s.size() - 2;
+        int val = (int)(n % period);
+        return s.get(val);
     }
 
 

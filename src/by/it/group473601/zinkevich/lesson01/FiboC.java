1d1f3768382d1f2ae7b347ed2bfca507199ecf6f
Helen <elenazinkevich05@gmail.com>
2017-02-12 23:18:21 +0300

    Lesson1 complete

--- src/by/it/group473601/zinkevich/lesson01/FiboC.java
+++ src/by/it/group473601/zinkevich/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group473601.zinkevich.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.util.ArrayList;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -25,7 +27,27 @@ public class FiboC {
         //решение практически невозможно найти интуитивно
         //вам потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
+
+
+        // находим ряд пизано
+        ArrayList<Long> arr = new ArrayList<>();
+        ArrayList<Long> arrFibo = new ArrayList<>();
+        arr.add((long)0);
+        arr.add((long)1);
+        arrFibo.add((long)0);
+        arrFibo.add((long)1);
+
+        for (int i = 2; i < 6 * m; i++){
+            arrFibo.add(i, arrFibo.get(i-1) + arrFibo.get(i-2));
+            arr.add(i, arrFibo.get(i)%m);
+            if (arr.get(i) == 1 && arr.get(i-1) == 0)
+                break;
+        }
+
+        int period = arr.size()-2;
+        int a = (int)n%period;
+        return arr.get(a);
+
     }
 
 

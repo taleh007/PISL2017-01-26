7e54aa0292dbf08cbdf92d3ae0b6a6a59a1067b2
Helen <elenazinkevich05@gmail.com>
2017-02-14 22:31:53 +0300

    Lesson2 TaskA complete

--- src/by/it/group473601/zinkevich/lesson02/A_VideoRegistrator.java
+++ src/by/it/group473601/zinkevich/lesson02/A_VideoRegistrator.java
@@ -1,6 +1,7 @@
 package by.it.group473601.zinkevich.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 даны события events
@@ -14,32 +15,31 @@ import java.util.List;
 public class A_VideoRegistrator {
 
     public static void main(String[] args) {
-        A_VideoRegistrator instance=new A_VideoRegistrator();
-        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        A_VideoRegistrator instance = new A_VideoRegistrator();
+        double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts = instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
     }
+
     //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration){
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
         result = new ArrayList<>();
-        int i=0;                              //i - это индекс события events[i]
-        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-                                              //подготовка к жадному поглощению массива событий
-                                              //hint: сортировка Arrays.sort обеспечит скорость алгоритма
-                                              //C*(n log n) + C1*n = O(n log n)
+        int i = 0;                              //i - это индекс события events[i]
 
-                                              //пока есть незарегистрированные события
-                                                //получим одно событие по левому краю
-                                                //и запомним время старта видеокамеры
-                                                //вычислим момент окончания работы видеокамеры
-                                                //и теперь пропустим все покрываемые события
-                                                //за время до конца работы, увеличивая индекс
+        Arrays.sort(events);
 
+        while(i < events.length){                        //пока есть незарегистрированные события
+            double startVideo = events[i];               //получим одно событие по левому краю
+            result.add(startVideo);                      //и запомним время старта видеокамеры
+            double endVideo = startVideo + workDuration; //вычислим момент окончания работы видеокамеры
+            i++;
+            while (i < events.length && events[i] <= endVideo)  //и теперь пропустим все покрываемые события
+                i++;
+        }
 
-
-        return result;                        //вернем итог
+        return result;
     }
 }

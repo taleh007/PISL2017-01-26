cca8cef932c34553325330ba05982a699b548100
sashokd <alexandra.dontsova99@gmail.com>
2017-02-26 23:24:37 +0200

    change package

---
+++ src/by/it/group473601/dontsova/lesson01/lesson1/FiboA.java
@@ -0,0 +1,59 @@
+package by.it.group473601.dontsova.lesson01.lesson1;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить рекурсивный способ вычисления чисел Фибоначчи
+ */
+
+public class FiboA {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        FiboA fibo = new FiboA();
+        int n = 33;
+        System.out.printf("calc(%d)=%d \n\t time=%d \n\n", n, fibo.calc(n), fibo.time());
+
+        //вычисление чисел фибоначчи медленным методом (рекурсией)
+        fibo = new FiboA();
+        n = 33;
+        System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
+    }
+
+
+    private int calc(int n) {
+        //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+        int number1=1;//f(n-2)
+        int number2=1;//f(n-1)
+        int number=1;//f(n)
+        if(n<=1)
+            return n;
+        for(int i=2;i<n;i++) {
+            number = number1 + number2;
+            number1 = number2;
+            number2 = number;
+        }
+        return number;
+    }
+
+
+    BigInteger slowA(Integer n) {
+        //рекурсия
+        //здесь нужно реализовать вариант без ограничения на размер числа,
+        //в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+        if(n<=1)
+            return BigInteger.valueOf(n);
+        else return slowA(n-1).add(slowA(n-2));
+    }
+
+
+
+}
+
---
+++ src/by/it/group473601/dontsova/lesson01/lesson1/FiboB.java
@@ -0,0 +1,37 @@
+package by.it.group473601.dontsova.lesson01.lesson1;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить способ вычисления чисел Фибоначчи с вспомогательным массивом
+ * без ограничений на размер результата (BigInteger)
+ */
+
+public class FiboB {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+
+        //вычисление чисел простым быстрым методом
+        FiboB fibo = new FiboB();
+        int n = 55555;
+        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
+    }
+
+    BigInteger fastB(Integer n) {
+        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
+        BigInteger[] arrayFib = new BigInteger[n+2];
+        arrayFib[0]=BigInteger.valueOf(0);
+        arrayFib[1]=BigInteger.valueOf(1);
+        for(int i=2;i<n+1;i++)
+            arrayFib[i]=arrayFib[i-1].add(arrayFib[i-2]);
+        return arrayFib[n];
+    }
+
+}
+

587df318fd72372b45136a3e2c5092cade49762d
taleh007 <z.taleh007@ya.ru>
2017-02-27 21:24:36 +0300

    Fix task A in lesson 2

--- src/by/it/group473601/zaliyev/lesson02/A_VideoRegistrator.java
+++ src/by/it/group473601/zaliyev/lesson02/A_VideoRegistrator.java
@@ -6,7 +6,6 @@ import java.util.*;
 реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
 заданный период времени (1) было минимальным, а все события events
 были зарегистрированы.
-
 Алгоритм жадный. Для реализации обдумайте надежный шаг.
 */
 
@@ -15,22 +14,23 @@ public class A_VideoRegistrator {
     public static void main(String[] args) {
         A_VideoRegistrator instance=new A_VideoRegistrator();
         double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts=instance.calcStartTimes(events,1);
-        System.out.println(starts);
+        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        System.out.println(starts);                            //покажем моменты старта
     }
-
+    //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration){
-        List<Double> result = new ArrayList<>();
+        List<Double> result;
+        result = new ArrayList<>();
         Arrays.sort(events);
-        double timeToOff = events[0];
-        double event;
-        for(int i = 1; i < events.length; i++) {
-            event = events[i];
-            if( timeToOff < event){
-                timeToOff = event + workDuration;
+        ArrayList<Integer> a = new ArrayList<>();
+        double time_to_off = -1;
+        for (int i = 0; i < events.length; i++) {
+            double event = events[i];
+            if( time_to_off < event){
+                time_to_off = event + workDuration;
                 result.add(event);
             }
         }
-        return result;
+        return result;                        //вернем итог
     }
 }

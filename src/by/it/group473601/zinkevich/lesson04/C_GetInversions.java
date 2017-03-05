e8de23098e2ab7340b606e7aeb21610b1134985d
Helen <elenazinkevich05@gmail.com>
2017-03-05 23:10:18 +0300

    Lesson4 complete

--- src/by/it/group473601/zinkevich/lesson04/C_GetInversions.java
+++ src/by/it/group473601/zinkevich/lesson04/C_GetInversions.java
@@ -3,6 +3,7 @@ package by.it.group473601.zinkevich.lesson04;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -35,27 +36,64 @@ Sample Output:
 
 public class C_GetInversions {
 
+    //рекурсивная функция сортировки частей массива
+    public static int[] sort(int[] array){
+        if(array.length < 2) {
+            return array;
+        }
+        int middle = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, middle);
+        int[] right = Arrays.copyOfRange(array, middle, array.length);
+        return merge(sort(left), sort(right));
+    }
+    //слияние двух массивов в один отсортированный
+    public static int[] merge(int[] left,int right[]){
+        int sizeResult = left.length + right.length;
+        int[] result = new int[sizeResult + 1];
+        int iLeft = 0;
+        int iRight = 0;
+
+        for(int i = 0; i < sizeResult; i++){
+            if(iLeft == left.length){
+                result[i] = right[iRight++];
+            }
+            else {
+                if (iRight == right.length) {
+                    result[i] = left[iLeft++];
+                }
+                else {
+                    if (left[iLeft] < right[iRight]) {
+                        result[i] = left[iLeft++];
+                    } else {
+                        if (left[iLeft] > right[iRight]) { // подсчет инверсий
+                            inversion += left.length - iLeft;
+                        }
+                        result[i] = right[iRight++];
+                    }
+                }
+            }
+        }
+        return result;
+        //return inversion;
+    }
+    static int inversion = 0;
+
     int calc(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!
         //размер массива
-        int n = scanner.nextInt();
+        int size = scanner.nextInt();
         //сам массив
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
         }
         int result = 0;
         //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
 
-
-
-
-
-
-
-
+        array = sort(array);
+        result = inversion;
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;

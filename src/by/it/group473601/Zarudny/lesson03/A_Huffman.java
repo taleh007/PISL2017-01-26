4ddf774001265f256251358460d4c96d53896049
zaruda <zaruda46@gmail.com>
2017-03-11 20:25:43 +0300

    fixed

--- src/by/it/group473601/Zarudny/lesson03/A_Huffman.java
+++ src/by/it/group473601/Zarudny/lesson03/A_Huffman.java
@@ -161,7 +161,7 @@ public class A_Huffman {
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        File f = new File(root + "by/it/a_khmelov/lesson03/dataHuffman.txt");
+        File f = new File(root + "by/it/group473601/Zarudny/lesson03/dataHuffman.txt");
         A_Huffman instance = new A_Huffman();
         long startTime = System.currentTimeMillis();
         String result = instance.encode(f);

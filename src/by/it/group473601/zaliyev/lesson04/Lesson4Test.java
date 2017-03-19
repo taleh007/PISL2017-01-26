5501b934bbc5403564355362ca055dc92f82a768
taleh007 <z.taleh007@ya.ru>
2017-03-19 22:29:54 +0300

    Refactor Lesson 4 Test

--- src/by/it/group473601/zaliyev/lesson04/Lesson4Test.java
+++ src/by/it/group473601/zaliyev/lesson04/Lesson4Test.java
@@ -12,50 +12,39 @@ public class Lesson4Test {
     @Test
     public void A() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson04/dataA.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group473601/zaliyev/lesson04/dataA.txt");
         A_BinaryFind instance = new A_BinaryFind();
-        //long startTime = System.currentTimeMillis();
-        int[] result=instance.findIndex(stream);
-        //long finishTime = System.currentTimeMillis();
-        StringBuilder sb=new StringBuilder();
-        for (int index:result){
+        int[] result = instance.findIndex(stream);
+        StringBuilder sb = new StringBuilder();
+        for (int index : result) {
             sb.append(index).append(" ");
         }
-        boolean ok=sb.toString().trim().equals("3 1 -1 1 -1");
+        boolean ok = sb.toString().trim().equals("3 1 -1 1 -1");
         assertTrue("A failed", ok);
     }
-
 
     @Test
     public void B() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson04/dataB.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group473601/zaliyev/lesson04/dataB.txt");
         B_MergeSort instance = new B_MergeSort();
-        //long startTime = System.currentTimeMillis();
-        int[] result=instance.getMergeSort(stream);
-        //long finishTime = System.currentTimeMillis();
-        boolean ok=result.length>3;
-        int test[]=new int[result.length];
-        System.arraycopy(result,0,test,0,result.length);
+        int[] result = instance.getMergeSort(stream);
+        boolean ok = result.length > 3;
+        int test[] = new int[result.length];
+        System.arraycopy(result, 0, test, 0, result.length);
         Arrays.sort(test);
-        for (int i = 0; i < result.length; i++) {
-            ok=ok && (result[i]==test[i]);
+        for (int i = 0; i < result.length && ok; i++) {
+            ok = (result[i] == test[i]);
         }
         assertTrue("B failed", ok);
     }
 
-
     @Test
     public void C() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson04/dataC.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group473601/zaliyev/lesson04/dataC.txt");
         C_GetInversions instance = new C_GetInversions();
-        //long startTime = System.currentTimeMillis();
-        int result = instance.calc(stream);
-        //long finishTime = System.currentTimeMillis();
-        boolean ok=(2==result);
-        assertTrue("C failed", ok);
-
+        long result = instance.calc(stream);
+        assertTrue("C failed", 2 == result);
     }
-
 }
